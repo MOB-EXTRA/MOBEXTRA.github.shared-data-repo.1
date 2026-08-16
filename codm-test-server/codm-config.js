e9659b136bf393125fd4f24d82fba5deca1c932c
MOB-EXTRA <marcjohnfordc@gmail.com>
2026-08-16 16:09:57 +0800

    Update shared config

--- codm-test-server/codm-config.js
+++ codm-test-server/codm-config.js
@@ -12,24 +12,15 @@ const testServerData = {
 
     links: [
         {
-            device: `
-            <i class="fa-brands fa-apple"></i> 
-            iOS (TestFlight)`,
-            icon: "codm-ts-logo-ios.png",
+            device: "iOS (TestFlight)",
             url: "https://testflight.apple.com/join/7M4pPKqb"
         },
         {
-            device: `
-            <i class="fa-brands fa-android"></i>
-            Android (32-bit)`,
-            icon: "codm-ts-logo-A32.png",
+            device: "Android (32-bit)",
             url: "https://ms.spr.ly/CODM_32bit_0611"
         },
         {
-            device: `
-            <i class="fa-brands fa-android"></i> 
-            Android (64-bit)`,
-            icon: "codm-ts-logo-A64.png",
+            device: "Android (64-bit)",
             url: "https://ms.spr.ly/CODM_64bit_0611"
         }
     ]
