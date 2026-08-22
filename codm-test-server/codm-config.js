79c9603b019039e89a5e8ff5ea2dc762d53aa503
MOB-EXTRA <marcjohnfordc@gmail.com>
2026-08-22 18:53:52 +0800

    Update shared config

--- codm-test-server/codm-config.js
+++ codm-test-server/codm-config.js
@@ -28,8 +28,8 @@ const testServerData = {
 };
 
 const notARobot = {
-    code: "1234",
+    code: "0822896",
     
     // Both the link and the embedded video will pull this exact source:
-    codeSource: "https://youtu.be/Tjf7v4LvU1g"
+    codeSource: "https://youtu.be/oRoUVGgtEpQ"
 };
