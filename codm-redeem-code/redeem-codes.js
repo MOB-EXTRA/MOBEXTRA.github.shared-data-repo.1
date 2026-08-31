d4892eddf4477ffdf7835bd49f6194d5d51b3d50
MOB-EXTRA <marcjohnfordc@gmail.com>
2026-08-31 11:05:48 +0800

    Update shared config

--- codm-redeem-code/redeem-codes.js
+++ codm-redeem-code/redeem-codes.js
@@ -1,6 +1,10 @@
 // Centralized CODM Redeem Codes Database (TOP NEWEST)
 const redeemCodesData = `
 
+r-code: PHWEEKEND2L4VA
+version: Garena
+data-added: Aug 31, 2026
+
 r-code: CODMTayoCN54D
 version: Garena
 data-added: Aug 29, 2026
